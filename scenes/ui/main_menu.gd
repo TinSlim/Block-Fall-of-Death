0c2678f3db1f7ee57d0f2c678467b432f941d4b4
TheAxz <jp.merelloz@gmail.com>
2022-09-03 14:12:25 -0400

    Main Menu Fixed

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -5,9 +5,9 @@ onready var credits = $VBoxContainer/Credits
 onready var exit = $VBoxContainer/Exit
 
 func _ready():
-	play.connect("pressed",self,"on_play_pressed")
-	credits.connect("pressed",self,"on_credits_pressed")
-	exit.connect("pressed",self,"on_exit_pressed")
+	play.connect("pressed",self,"_on_play_pressed")
+	credits.connect("pressed",self,"_on_credits_pressed")
+	exit.connect("pressed",self,"_on_exit_pressed")
 
 func _on_play_pressed():
 	get_tree().change_scene("res://scenes/nivel.tscn")
